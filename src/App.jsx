982c99b5273262190c27aeb5433cba6f29c6dd1c
Mudrika Chawla <chawlamudrika202001@gmail.com>
2023-07-21 11:58:23 +0530

    Caraousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import './App.css';
 import Companies from "./components/Companies/Companies";
+import Caraousel from "./components/Caraousel/Caraousel";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
       </div>
       <Companies/>
+      <Caraousel/>
     </div>
   );
 };
